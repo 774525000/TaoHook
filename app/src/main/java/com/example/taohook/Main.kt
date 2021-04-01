3ed54c9913af883f51b420104f4d5d0e6c496ca7
root <774525000@qq.com>
2021-04-01 22:30:21 +0800

    ������✌✌

---
+++ app/src/main/java/com/example/taohook/Main.kt
@@ -0,0 +1,31 @@
+package com.example.taohook
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class Main : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
+        lpparam?.apply {
+            XposedHelpers.findAndHookMethod(
+                    "mtopsdk.mtop.global.SwitchConfig",
+                    classLoader,
+                    "isGlobalSpdySwitchOpen",
+                    MainHook()
+            )
+        }
+    }
+}
+
+
+class MainHook : XC_MethodHook() {
+    override fun afterHookedMethod(param: MethodHookParam?) {
+        super.afterHookedMethod(param)
+        XposedBridge.log("运行到我啦")
+        param?.apply {
+            result = false
+        }
+    }
+}
